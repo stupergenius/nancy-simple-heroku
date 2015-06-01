c99adfc9eb7e2336b3418cb0cc4c8f934103efaf
Ben Snider <benjamin.snider@gmail.com>
2015-06-01 17:22:22 -0600

    Adding SMS route.

--- src/MainModule.cs
+++ src/MainModule.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text;
 
 namespace Nancy.Simple
 {
@@ -8,6 +9,18 @@ namespace Nancy.Simple
 		{
 			Get["/"] = args => {
 				return View ["index", Request.Url];
+			};
+			Post["/sms"] = args => {
+				var twiml = "<Response><Message>Hello World!</Message></Response>";
+				return new Response
+				{
+					StatusCode = HttpStatusCode.OK,
+					ContentType = "text/xml",
+					Contents = (stream) => {
+						var message = Encoding.UTF8.GetBytes(twiml);
+						stream.Write(message, 0, message.Length);
+					},
+				};
 			};
 		}
 	}
